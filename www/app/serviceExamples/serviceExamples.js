e09da463041924ed8debe54a0d2d45218376d82e
John Battista <jbattista@expedia.com>
2015-06-04 15:12:23 -0700

    Service and factory example

--- www/app/serviceExamples/serviceExamples.js
+++ www/app/serviceExamples/serviceExamples.js
@@ -4,11 +4,29 @@
 	angular.module('serviceExamples', [])
 		.constant('API_ROOT', 'http://example.com')
 		.value('sharedValue', { widgetCount: 0})
-		.controller('ServicesController', function($scope, API_ROOT, sharedValue) {
+		.service('stringUtilsService', function(){
+			this.makeAwesome = function(s) {
+				return s + " is awesome!";
+			};			
+		})
+		.factory('stringUtilsFactory', function(){
+			return new function(){
+				this.makeAwesome = function(s) {
+					return s + " is awesome!";
+				};			
+			}
+		})
+		.controller('ServicesController', function($scope, API_ROOT, sharedValue, 
+				stringUtilsService, stringUtilsFactory) {
+
 			var vm = this;
 
 			vm.API_ROOT = API_ROOT;
 			vm.sharedValue = sharedValue;
+
+			vm.original = 'everything';
+			vm.after = stringUtilsService.makeAwesome(vm.original);
+
 		})
 		.controller('Controller1', function(sharedValue) {
 			var vm = this;
