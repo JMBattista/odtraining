8c7c4c6009ed4c7614c676f0d59e8378f2719924
John Battista <jbattista@expedia.com>
2015-06-04 15:00:25 -0700

    Shared value example

--- www/app/serviceExamples/serviceExamples.js
+++ www/app/serviceExamples/serviceExamples.js
@@ -3,10 +3,23 @@
 
 	angular.module('serviceExamples', [])
 		.constant('API_ROOT', 'http://example.com')
-		.controller('ServicesController', function($scope, API_ROOT) {
+		.value('sharedValue', { widgetCount: 0})
+		.controller('ServicesController', function($scope, API_ROOT, sharedValue) {
 			var vm = this;
 
 			vm.API_ROOT = API_ROOT;
+			vm.sharedValue = sharedValue;
 		})
+		.controller('Controller1', function(sharedValue) {
+			var vm = this;
+
+			vm.sharedValue = sharedValue;
+		})
+		.controller('Controller2', function(sharedValue) {
+			var vm = this;
+
+			vm.sharedValue = sharedValue;
+		})
+
 	;
 })();
